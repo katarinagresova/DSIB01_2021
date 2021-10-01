8065c621eeabb090edbf2f5f791ccd566d20e794
Katarina Gresova <514001@mail.muni.cz>
2021-10-01 10:16:26 +0200

    Adding demo script

---
+++ assets/files/download.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#TODO: add description
+
+BASE_URL=https://www.encodeproject.org/files
+
+#TODO: insert names of your files
+FILES=(ENCFF959XKN)
+
+mkdir data
+cd data
+
+for file in $FILES
+do
+	# creating url of file
+	url=$BASE_URL/$file/@@download/$file.fastq.gz
+	echo File URL: $url
+
+	#TODO: download file. hint: look at command wget
+	
+	#unzip downloaded file. zipped version is automaticaly deleted.
+	gunzip $file.fastq.gz
+done
+
+echo Downloaded files:
+ls
